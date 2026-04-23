faadfadfece788c3b2a7beb74b8f5f8b75609b8f
SweepTosher <sweeptosher@gmail.com>
2026-04-23 21:37:08 +0800

    guard
    merge

--- module/umamusume/script/cultivate_task/ui_handlers.py
+++ module/umamusume/script/cultivate_task/ui_handlers.py
@@ -99,10 +99,10 @@ def script_main_menu(ctx: UmamusumeContext):
 
 def script_scenario_select(ctx: UmamusumeContext):
     img_gray = ctx.ctrl.get_screen(to_gray=True)
-
     next_match = image_match(img_gray, REF_NEXT)
     if next_match.find_match and next_match.center_point[0] > 400:
         return
+
 
     if image_match(img_gray, UI_CULTIVATE_UMAMUSUME_SELECT).find_match or \
        image_match(img_gray, UI_CULTIVATE_EXTEND_UMAMUSUME_SELECT).find_match or \
@@ -138,7 +138,6 @@ def script_umamusume_select(ctx: UmamusumeContext):
         return
     time.sleep(2)
     click_next_button(ctx, prefer_right=True)
-
 
 
 def script_extend_umamusume_select(ctx: UmamusumeContext):
