4101df794d83f831aa3c41b564f25bd2b63f1c9d
Loki <271817881+loki-cmyk@users.noreply.github.com>
2026-07-11 21:43:11 -0400

    Handle Unity Splash banner after DroidCast speedups.

--- module/umamusume/script/cultivate_task/parse.py
+++ module/umamusume/script/cultivate_task/parse.py
@@ -481,8 +481,7 @@ def parse_train_main_menu_operations_availability(ctx: UmamusumeContext, img):
     rest_available = btn_rest_check_point[0] > 200
     train_available = btn_train_check_point[0] > 200
     skill_available = btn_skill_check_point[0] > 200
-    if btn_medic_room_check_point[0] > 200 and btn_medic_room_check_point[1] > 200 and btn_medic_room_check_point[
-        2] > 200:
+    if btn_medic_room_check_point[0] > 200 and btn_medic_room_check_point[1] > 200 and btn_medic_room_check_point[2] > 200:
         medic_room_available = True
     else:
         medic_room_available = False
